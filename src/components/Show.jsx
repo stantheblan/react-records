1449cab08473625a2193fce5e41544f908eda3d4
Stan <stantonmoore42@gmail.com>
2022-04-05 15:56:07 -0400

    Delete Working

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,18 +1,26 @@
 import { React, useState, useEffect } from 'react'
-import { getRecord } from '../services/records-api'
-import { useParams } from 'react-router-dom'
+import { deleteRecord, getRecord } from '../services/records-api'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export function Show() {
   const { id } = useParams()
+  const nav = useNavigate();
   const [data, setData] = useState({})
 
   useEffect(() => {
       getRecord(id).then(res => setData(res.data))
   }, [])
+
+  const deleteTheRecord = () => {
+    deleteRecord(id)
+    nav("/")
+  }
   
   return (
     <div>
         <h1>{data.title}</h1><br/>
+        <br/>
+        <button onClick={deleteTheRecord}>Delete</button>
     </div>
   )
 }
