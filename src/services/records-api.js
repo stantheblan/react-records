261fe204887978691297e66119fbbf23961df18a
Stan <stantonmoore42@gmail.com>
2022-04-05 15:42:02 -0400

    add getRecord
    singular

--- src/services/records-api.js
+++ src/services/records-api.js
@@ -3,6 +3,10 @@ export const createRecord = item => {
     const URL = `http://localhost:3001/records/`
     return axios.post(URL, item)
 }
+export const getRecord = id => {
+    const URL = `http://localhost:3001/records/${id}`
+    return axios.get(URL)
+}
 export const getRecords = () => {
     const URL = `http://localhost:3001/records`
     return axios.get(URL)
