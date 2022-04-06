bf43495075b3ab3fa2f2a83325ead29ec6573cd4
Stan <stantonmoore42@gmail.com>
2022-04-05 23:22:17 -0400

    Add Edit Route

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import { Records } from './components/Records';
 import { New } from './components/New';
 import { Show } from './components/Show';
+import { Edit } from './components/Edit';
 
 export default function App() {
   return (
     <div className="App">
       <Router> 
         <nav>
-          <Link to='/'>Home</Link>  
-          <Link to='/new'>New</Link>
+          <Link to='/'> ~Home </Link>  
+          <Link to='/new'> ~New</Link>
         </nav>
         <Routes>
           <Route path='/' element={<Records/>}></Route>
           <Route path='/:id' element={<Show/>}></Route>
           <Route path='/new' element={<New/>}></Route>
+          <Route path='/:id/edit' element={<Edit/>}></Route>
         </Routes>
       </Router>
     </div>
